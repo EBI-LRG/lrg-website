a117065d0c8121d70033e51c0791f5f6c425389f
Laurent Gil <lgil@ebi.ac.uk>
2016-04-05 14:21:23 +0100

    Remove the FaqGroupMapGenerator method

--- _plugins/stub_name_generator.rb
+++ _plugins/stub_name_generator.rb
@@ -30,21 +30,5 @@ module Jekyll
       site.data['faq_category_map'] = faq_category_map
     end
   end
-
-  class FaqGroupMapGenerator < Jekyll::Generator
-    safe true
-    priority :lowest
-
-    def generate(site)
-
-      faq_group_map = Array.new
-      for faq_group in site.collections['faq_groups'].docs
-        faq_group_stub = faq_group.path[/([^\/]*)\.[^\.\/]*$/, 1]
-        faq_group.data['stub_name'] = faq_group_stub
-        faq_group_map.push(faq_group.to_liquid);
-      end
-      site.data['faq_group_map'] = faq_group_map
-    end
-  end
         
 end
